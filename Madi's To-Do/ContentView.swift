717f243d9b16e9b6e6716d35bdad68a66425447d
Matthew Condie <matthew.condie@gmail.com>
2020-01-27 18:17:21 -0700

    Initial Commit

---
+++ Madi's To-Do/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Madi's To-Do
+//
+//  Created by Matthew Condie on 1/27/20.
+//  Copyright © 2020 mcondie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
